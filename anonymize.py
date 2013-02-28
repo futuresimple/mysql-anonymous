70bfaded8f7d888aef40644bc27124360c2d6a74
Karol Nowak <karol@futuresimple.com>
2013-02-28 14:38:29 +0100

    Snapshotting development during a longer break.

--- anonymize.py
+++ anonymize.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # This assumes an id on each field.
-import logging
 
+import logging
+import sys
+
+import MySQLdb
+import yaml
 
 log = logging.getLogger('anonymize')
 
@@ -69,6 +73,57 @@ def get_updates(config):
     return sql
 
 
+def check_configuration(config):
+    connection_info = config['connection']
+    connection = MySQLdb.connect(
+        host=connection_info.get('host', 'localhost'),
+        user=connection_info['username'],
+        passwd=connection_info['password'],
+        db=connection_info['database'],
+    )
+
+    maintenance_connection = MySQLdb.connect(
+        host=connection_info.get('host', 'localhost'),
+        user=connection_info['username'],
+        passwd=connection_info['password'],
+        db="information_schema",
+    )
+
+    database = config.get('database', {})
+    tables = database.get('tables', [])
+
+    for table in tables:
+        configured_columns = set()
+        for operations in tables[table].values():
+            configured_columns.update(operations)
+
+        maintenance = maintenance_connection.cursor()
+        maintenance.execute('''
+            SELECT COLUMN_NAME 
+            FROM COLUMNS 
+            WHERE 
+                SCHEMA_NAME = '%(schema)s' AND 
+                TABLE_NAME = '%(table)s'
+        ''' % {
+            'schema': config['database'],
+            'table': table
+        })
+
+        existing_columns = maintenance.fetchall()
+        missed_columns = existing_columns - configured_columns
+        extra_columns = configured_columns - existing_columns
+
+        if missed_columns:
+            logger.error("All columns must be specified in configuration.")
+            logger.error("Missing columns are: %s" % (missed_collumns, ))
+            sys.exit(1)
+
+        if extra_columns:
+            logger.error("Your configuration names columns that don't exists in the database.")
+            logger.error("These columns are: %s" % (extra_columns, ))
+            sys.exit(1)
+
+
 def anonymize(config):
     sql = []
     sql.extend(get_truncates(config))
@@ -81,8 +136,15 @@ def anonymize(config):
 
 
 if __name__ == '__main__':
-    import yaml
-    import sys
-    f = sys.argv[1] if len(sys.argv) > 1 else 'anonymize.yml'
-    cfg = yaml.load(open(f))
-    anonymize(cfg)
+    db_file_names = sys.argv[1:]
+    if not db_file_names:
+        logger.error("Usage: %s config_file [config_file ...]", sys.argv[0])
+
+    for db_file_name in db_file_names:
+        logger.info("Processing %s", db_file_name)
+        with open(db_file_name) as db_file:
+            cfg = yaml.loads(db_file)
+
+        check_config(fg)
+        anonymize(cfg)
+
